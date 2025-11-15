d2a087048b7c2871222fb6c91f9544ab548635ec
Dhruv Jain <dhruvxjain@gmail.com>
2025-11-15 18:37:20 +0530

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// const clickHandler = (msg) => {
+//     alert(msg);
+// }
 
-function App() {
-  const [count, setCount] = useState(0)
+// const wrapHandler = () => {
+//     clickHandler("Hello");
+// }
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+var a = 20;
+var b = "Hello"
+var c = true;
+var d = null;
+var e = undefined;
+var f = 2.3333
+
+const App = () => {
+
+    return(
+        <div>
+            <h1>App</h1>
+            <h2>{a}</h2>
+            <h2>String : {b}</h2>
+            <h2>Boolean : {c}</h2>
+            <h2>Null : {d}</h2>
+            <h2>UD : {e}</h2>
+            <h2>Float : {f}</h2>
+        </div>
+    )
+
 }
 
-export default App
+export default App;
